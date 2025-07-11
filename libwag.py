31c460ca6a3e1d620889afc0465cdd53f603a43f
Low-go <lorranalvesgaldino98@gmail.com>
2025-07-10 22:03:45 -1000

    initialize command line parsers

--- libwag.py
+++ libwag.py
@@ -9,3 +9,20 @@ import os
 import re
 import sys
 import zlib
+
+'''
+The libraries we have are for the following
+
+* Parsing command line arguments
+* Simple datetime to keep track of commits
+* fnmatch to match file extensions, this will be used in gitignore
+* haslib creates unique id for commits
+* zlib compression algorith. Make file size smaller when commiting/pushing I bet
+
+'''
+
+
+argpaser = argparse.ArgumentParser(description="A content parser")
+
+argsubparsers = argpaser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
