f5abfb35e530d053c3f4dedf10e59f3054c3b0bf
Low-go <lorranalvesgaldino98@gmail.com>
2025-07-10 22:11:29 -1000

    switch case for possible sub commands for git, will be routed to respective functions

--- libwag.py
+++ libwag.py
@@ -22,7 +22,27 @@ The libraries we have are for the following
 '''
 
 
-argpaser = argparse.ArgumentParser(description="A content parser")
+argparser = argparse.ArgumentParser(description="A content parser")
 
-argsubparsers = argpaser.add_subparsers(title="Commands", dest="command")
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command") # grabs second, thirs and so on command line argumemnts
 argsubparsers.required = True
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add"          : cmd_add(args)
+        case "cat-file"     : cmd_cat_file(args)
+        case "check-ignore" : cmd_check_ignore(args)
+        case "checkout"     : cmd_checkout(args)
+        case "commit"       : cmd_commit(args)
+        case "hash-object"  : cmd_hash_object(args)
+        case "init"         : cmd_init(args)
+        case "log"          : cmd_log(args)
+        case "ls-files"     : cmd_ls_files(args)
+        case "ls-tree"      : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        case "rm"           : cmd_rm(args)
+        case "show-ref"     : cmd_show_ref(args)
+        case "status"       : cmd_status(args)
+        case "tag"          : cmd_tag(args)
+        case _              : print("Bad command.")
